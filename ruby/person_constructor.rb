70ea8b6f2e56afaf88184d8ab450c1f4ae1bcb6a
Michael Silberstein <mikesilb123@gmail.com>
2017-10-17 14:09:52 -0400

    Refactors Person Constructor program

--- ruby/person_constructor.rb
+++ ruby/person_constructor.rb
@@ -3,15 +3,12 @@ class Person
   def initialize(name, last_name = nil, middle_name = nil)
     if last_name == nil && middle_name == nil
       name_arr = name.split(' ')
+      @first_name = name_arr[0]
       if name_arr.length == 3
-        @first_name = name_arr[0]
         @middle_name = name_arr[1]
         @last_name = name_arr[2]
       elsif name_arr.length == 2
-        @first_name = name_arr[0]
         @last_name = name_arr[1]
-      elsif name_arr.length == 1
-        @first_name = name_arr[0]
       end
     elsif middle_name == nil
       @first_name = name
