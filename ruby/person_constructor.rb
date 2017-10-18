b861214a9fae0a535486ae4e3098fc9e3aa37fc0
Michael Silberstein <mikesilb123@gmail.com>
2017-10-17 20:18:39 -0400

    Refactors Person Constructor program some more

--- ruby/person_constructor.rb
+++ ruby/person_constructor.rb
@@ -1,8 +1,9 @@
+require 'pry'
 class Person
   attr_accessor :first_name, :last_name, :middle_name
   def initialize(name, last_name = nil, middle_name = nil)
-    if last_name == nil && middle_name == nil
-      name_arr = name.split(' ')
+    if last_name.nil? && middle_name.nil?
+      name_arr = name.split
       @first_name = name_arr[0]
       name_arr.shift
       if name_arr.length >= 1
@@ -18,3 +19,4 @@ class Person
     end
   end
 end
+binding.pry
