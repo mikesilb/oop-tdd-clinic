36da42b3a765f62064269699bdbe1dd2f721960c
Michael Silberstein <mikesilb123@gmail.com>
2017-10-23 14:38:02 -0400

    Grocery List Basic Javascript lesson - Meets Expectations level

---
+++ javascript/grocery-list-basic-javascript/src/GroceryList.js
@@ -0,0 +1,1 @@
+// For Exceeds Expectations Only
---
+++ javascript/grocery-list-basic-javascript/src/main.js
@@ -0,0 +1,49 @@
+// Meets Expectations
+let bestieGroceryItems = [new GroceryItem("apple", .50, 5), new GroceryItem("avocado", 2.00, 2), new GroceryItem("beer", 2.50, 4), new GroceryItem("kale", 2.00) ];
+
+summary = (GroceryItems) => {
+  let sum = 0;
+  let output = "Dear Bestie, \n\n";
+  output += "I bought you: \n\n";
+
+  GroceryItems.forEach((item => {
+    let product = item.price * item.quantity;
+    output += item.quantity + " " + item.name + "s" + " - " + "$" + product.toFixed(2) + "\n";
+    sum += product;
+  }));
+
+  output += "\n\nYou owe me " + sum.toFixed(2)+".";
+
+  return output;
+};
+
+console.log(summary(bestieGroceryItems));
+
+
+
+// Exceeds Expectations
+let inventoryNearMe = {
+  banana: 2,
+  coffee: 2,
+  kale: 5,
+  chocolateBar: 5,
+  apple: 10,
+  potato: 11,
+  tomato: 1,
+  beer: 3,
+  avocado: 1,
+  milk: 5
+};
+
+let inventoryNearBestie = {
+  banana: 2,
+  coffee: 2,
+  kale: 5,
+  chocolateBar: 5,
+  apple: 10,
+  potato: 11,
+  tomato: 1,
+  beer: 6,
+  avocado: 10,
+  milk: 5
+};
