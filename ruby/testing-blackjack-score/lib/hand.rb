e6a8d3d44d7f79bd9c65aab8c073599c9a22f509
Michael Silberstein <mikesilb123@gmail.com>
2017-10-29 14:10:36 -0400

    Testing Blackjack Score lesson

---
+++ ruby/testing-blackjack-score/lib/hand.rb
@@ -0,0 +1,23 @@
+require_relative 'deck'
+
+class Hand
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def calculate_hand
+    sum = 0
+    @cards.each do |card|
+      if card.rank.class == Fixnum
+        sum += card.rank
+      elsif card.rank.class == String && card.rank != "A"
+        sum += 10
+      elsif sum <= 10
+        sum += 11
+      else
+        sum += 1
+      end
+    end
+    sum
+  end
+end
