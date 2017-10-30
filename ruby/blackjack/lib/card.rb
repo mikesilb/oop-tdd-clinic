858e362d553b2f7c8d235e557ed45357553d66be
Michael Silberstein <mikesilb123@gmail.com>
2017-10-30 12:07:59 -0400

    Blackjack lesson #2 - Adds code to make Card unit tests pass

--- ruby/blackjack/lib/card.rb
+++ ruby/blackjack/lib/card.rb
@@ -1,3 +1,7 @@
 class Card
-  # Your code here
+  attr_reader :suit, :rank
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+  end
 end
--- ruby/blackjack/spec/lib/card_spec.rb
+++ ruby/blackjack/spec/lib/card_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Card do
     expect(card1.suit).to eq "♦"
     expect(card1.rank).not_to eq 7
     expect(card1.rank).not_to eq "K"
-    expect(card1.rank).to eq "2"
+    expect(card1.rank).to eq 2
     expect(card2.suit).to eq "♥"
     expect(card3.suit).to eq "♥"
     expect(card4.suit).to eq "♥"
     expect(card2.rank).to eq 2
     expect(card3.rank).to eq 10
-    expect(card3.rank).to eq "K"
+    expect(card4.rank).to eq "K"
   end
 end
