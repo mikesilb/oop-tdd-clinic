c10ab9ac9e2474ae400470a9324de073acc523b8
Michael Silberstein <mikesilb123@gmail.com>
2017-10-30 12:03:20 -0400

    Blackjack lesson #1 - Adds initial Card unit tests

--- ruby/blackjack/spec/lib/card_spec.rb
+++ ruby/blackjack/spec/lib/card_spec.rb
@@ -1,10 +1,10 @@
 require "spec_helper"
 
 RSpec.describe Card do
-  let(:card1) = Card.new("♦",2)
-  let(:card2) = Card.new("♥",2)
-  let(:card3) = Card.new("♥",10)
-  let(:card4) = Card.new("♥","K")
+  let(:card1) { Card.new("♦",2) }
+  let(:card2) { Card.new("♥",2) }
+  let(:card3) { Card.new("♥",10) }
+  let(:card4) { Card.new("♥","K") }
   # Your tests here
   it "generates a distintive suit and rank for each card" do
     expect(card1.suit).not_to eq "♥"
