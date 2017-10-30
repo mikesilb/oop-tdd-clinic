cec6b5869715fc5d4955f067bb1c1066dd0fdd69
Michael Silberstein <mikesilb123@gmail.com>
2017-10-30 13:53:01 -0400

    Blackjack lesson #3 - Adds Deck tests and code to make them pass

--- ruby/blackjack/lib/deck.rb
+++ ruby/blackjack/lib/deck.rb
@@ -1,3 +1,20 @@
+require_relative 'card'
 class Deck
-  # Your code here
+  attr_reader :the_deck
+  def initialize
+    @the_deck = [];
+    suit_list = ['♦', '♣', '♠', '♥']
+    rank_list = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+
+    suit_list.each do | suit |
+      rank_list.each do | rank |
+        @the_deck.push(Card.new(suit, rank))
+      end
+    end
+    the_deck.shuffle!
+  end
+
+  def deal(num_cards)
+    @the_deck.pop(num_cards)
+  end
 end
--- ruby/blackjack/spec/lib/deck_spec.rb
+++ ruby/blackjack/spec/lib/deck_spec.rb
@@ -1,5 +1,25 @@
 require "spec_helper"
 
 RSpec.describe Deck do
-  # Your tests here
+  let(:deck) { Deck.new }
+  describe "deck" do
+    it "consists of 52 unique cards" do
+      expect(deck.the_deck.length).to eq 52
+      expect(deck.the_deck.uniq.length).to eq 52
+    end
+  end
+  describe "#deal" do
+    it "will remove a certain number of cards from the deck" do
+      untouched_deck_size = deck.the_deck.length
+      deck.deal(2)
+      expect(deck.the_deck.length).to eq (untouched_deck_size - 2)
+    end
+    it "will remove card from the top of the deck" do
+      top_card = deck.the_deck[-1]
+      bottom_card = deck.the_deck[0]
+      the_dealed_card = deck.deal(1)
+      expect(the_dealed_card).to eq [top_card]
+      expect(the_dealed_card).not_to eq [bottom_card]
+    end
+  end
 end
