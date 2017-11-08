7718633dbbaa3910b0d961a20a605b96c7c16717
Michael Silberstein <mikesilb123@gmail.com>
2017-11-08 16:21:24 -0500

    Connect4 lesson #5 - Added basic Board functionality

---
+++ ruby/connect-4/lib/board.rb
@@ -0,0 +1,19 @@
+require_relative './position'
+class Board
+  attr_reader :width, :height, :the_grid
+  def initialize(width = 7, height = 6)
+    @width = width
+    @height = height
+    x=0
+    @the_arr = [];
+    while x < @width
+      y=0
+      while y < @height
+        @the_grid << Position.new([x,y])
+        y+=1
+      end
+      x+=1
+    end
+  end
+end
+the_new_board = Board.new
