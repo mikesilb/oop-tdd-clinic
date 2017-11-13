7de0c440c4e5c6d8e2f9f87697cb9ec9a897a963
Michael Silberstein <mikesilb123@gmail.com>
2017-11-13 18:41:02 -0500

    Added functionality to Connect4 class and the running of the game

--- ruby/connect-4/lib/player.rb
+++ ruby/connect-4/lib/player.rb
@@ -1,7 +1,7 @@
 class Player
   attr_reader :name, :age, :skill_level
   attr_accessor :num_chips_remain
-  def initialize(name, age, skill_level, num_chips_remain = 21)
+  def initialize(name, age = nil, skill_level = nil, num_chips_remain = 21)
     @name = name
     @age = age
     @skill_level = skill_level
