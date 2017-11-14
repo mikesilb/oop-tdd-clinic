ceaf10fee570eee3d54eb8f8280d0a36451bafc3
Michael Silberstein <mikesilb123@gmail.com>
2017-11-14 12:17:17 -0500

    Adds passing tests for horizontal and vertical connect4 checks

--- ruby/connect-4/lib/board.rb
+++ ruby/connect-4/lib/board.rb
@@ -1,3 +1,4 @@
+require_relative './position'
 class Board
   attr_reader :column, :row, :the_grid
   def initialize(column = 7, row = 6)
@@ -53,6 +54,21 @@ class Board
     available
   end
 
+  def has_a_chip?(column, row, symbol)
+    result = nil
+    @the_grid.each do | the_location |
+      if the_location.location == [column, row]
+        if the_location.availability == false && the_location.chip_at_position.symbol == symbol
+          result = true
+        else
+          result = false
+        end
+      end
+    end
+    result
+  end
+
+
   def occupying_the_position(column, row, chip)
     the_grid.each do | the_location |
       if (the_location.location == [column, row])
@@ -61,4 +77,54 @@ class Board
     end
   end
 
+  def is_a_horizontal_connect4?(row, symbol)
+    horizontal_connect4 = nil
+    column = 0
+    while column < @column - 3
+      if has_a_chip?(column, row, symbol) == true
+        if has_a_chip?(column+1, row, symbol) == true
+          if has_a_chip?(column+2, row, symbol) == true
+            if has_a_chip?(column+3, row, symbol) == true
+              horizontal_connect4 = true
+            end
+          end
+        end
+      end
+      column += 1
+    end
+    if horizontal_connect4 == nil
+      horizontal_connect4 = false
+    end
+    horizontal_connect4
+  end
+
+  def is_a_vertical_connect4?(column, symbol)
+    vertical_connect4 = nil
+    row = 0
+    while row < @row - 3
+      if has_a_chip?(column, row, symbol) == true
+        if has_a_chip?(column, row+1, symbol) == true
+          if has_a_chip?(column, row+2, symbol) == true
+            if has_a_chip?(column, row+3, symbol) == true
+              vertical_connect4 = true
+            end
+          end
+        end
+      end
+      row += 1
+    end
+    if vertical_connect4 == nil
+      vertical_connect4 = false
+    end
+    vertical_connect4
+  end
+
+  def is_a_connect4?
+    is_connect4 = nil
+    #NEEDS TO FILL IN A TON OF STUFF HERE
+    is_connect4
+  end
+
 end
+# b1 = Board.new
+# binding.pry
