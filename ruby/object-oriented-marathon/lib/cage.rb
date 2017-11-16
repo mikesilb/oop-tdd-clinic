3360232e83433fe506df4291aac0913fe0e4b124
Michael Silberstein <mikesilb123@gmail.com>
2017-11-16 00:13:01 -0500

    Object Oriented Marathon #2 - Fifteen tests passed thus far

---
+++ ruby/object-oriented-marathon/lib/cage.rb
@@ -0,0 +1,14 @@
+require_relative './gorilla'
+class Cage
+  attr_accessor :animal
+  def initialize
+    @animal = nil
+  end
+  def empty?
+    if @animal == nil
+      true
+    else
+      false
+    end
+  end
+end
