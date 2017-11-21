e0c8d84fcca5e68100c364b529a2e6c782add0a3
Michael Silberstein <mikesilb123@gmail.com>
2017-11-21 15:25:52 -0500

    Cohort Statistics Lesson #3: Passed 41 tests....Completed '06' spec file

--- ruby/cohort-stats/lib/system_check.rb
+++ ruby/cohort-stats/lib/system_check.rb
@@ -1,3 +1,5 @@
+require_relative './grade'
+include Grade
 class SystemCheck
   attr_reader :submissions
   def initialize(name, date)
@@ -8,5 +10,24 @@ class SystemCheck
   def add_submission(submission)
     @submissions.push(submission)
   end
-
+  def average_grade
+    sum = 0
+    @submissions.each do |submission|
+      sum += submission.grade.to_f
+    end
+    average = sum/@submissions.length
+  end
+  def did_student_complete_system_check?(student)
+    match = nil
+    @submissions.each do |submission|
+      if student.name == submission.student.name
+        match = true
+      end
+    end
+    if match == nil
+      false
+    elsif match == true
+      true
+    end
+  end
 end
