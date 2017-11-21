7e9015186bd66974f725ab65073e50b94018e8a9
Michael Silberstein <mikesilb123@gmail.com>
2017-11-21 14:48:38 -0500

    Cohort Statistics Lesson #2: Passed 38 tests....Completed '05' spec file

--- ruby/cohort-stats/lib/system_check_submission.rb
+++ ruby/cohort-stats/lib/system_check_submission.rb
@@ -1,8 +1,25 @@
+require_relative './grade'
+include Grade
 class SystemCheckSubmission
-  attr_reader :solution, :student
+  attr_reader :solution, :student, :grade
   def initialize(solution, student)
     @solution = solution
     @student = student
+    @grade = nil
+  end
+  def assign_grade(grade)
+    if !GRADES.include?(grade)
+      raise InvalidGradeError, "Impossible grade!"
+    else
+      @grade = grade
+    end
+  end
+  def graded?
+    if @grade != nil
+      true
+    else
+      false
+    end
   end
 end
 
