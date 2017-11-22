96b94c6d013bb88a783ffb2377facadab8c93a3f
Michael Silberstein <mikesilb123@gmail.com>
2017-11-22 00:14:55 -0500

    Cohort Statistics Lesson #4: Passed all tests....Exercise completed.

--- ruby/cohort-stats/lib/cohort.rb
+++ ruby/cohort-stats/lib/cohort.rb
@@ -27,4 +27,24 @@ class Cohort
     end
     the_roster
   end
+  def system_check_completed?(system_check)
+    all_submitted = nil
+    list_of_submission_names = []
+
+    system_check.submissions.each do |submission|
+      list_of_submission_names.push(submission.student.name)
+    end
+
+    @students.each do |student|
+      if !list_of_submission_names.include?(student.name)
+        all_submitted = false
+      end
+    end
+
+    if all_submitted == nil
+      true
+    else
+      false
+    end
+  end
 end
